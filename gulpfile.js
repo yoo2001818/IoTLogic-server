02ea08909d9c3c8aba4a5b4fd7d4708dba893bce
Duknam Yoo <yoo2001818@gmail.com>
2016-07-12 23:25:25 +0900

    Add boilerplate for client implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
-var babel = require('gulp-babel');
-var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
-require('babel-register');
+var gutil = require('gulp-util');
+var eslint = require('gulp-eslint');
+var babel = require('gulp-babel');
+var webpack = require('webpack');
+var webpackConfiguration = require('./webpack.config.js');
+var del = require('del');
+require('babel-core/register');
 
-// TODO: Add code coverage tool
-
+/*** Unit tests ***/
 gulp.task('lint', function () {
-  return gulp.src(['src/**/*.js', 'client-test/**/*.js'])
+  return gulp.src(['src/**/*.js', 'test/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
@@ -15,30 +18,55 @@ gulp.task('lint', function () {
 
 gulp.task('mocha', function() {
   return gulp.src(['test/**/*.js'], { read: false })
-    .pipe(mocha({ reporter: 'spec' }));
+    .pipe(mocha({ reporter: 'spec' }))
+    .on('error', gutil.log);
 });
 
-gulp.task('mochaSimple', function() {
+gulp.task('mochaMin', function() {
   return gulp.src(['test/**/*.js'], { read: false })
-    .pipe(mocha({ reporter: 'min' }));
+    .pipe(mocha({ reporter: 'min' }))
+    .on('error', gutil.log);
 });
 
 gulp.task('test', ['lint', 'mocha']);
 
-gulp.task('watch', function() {
-  return gulp.watch(['src/**/*.js', 'test/**/*.js'], ['mochaSimple']);
+/*** Frontend ***/
+gulp.task('webpack', function(callback) {
+  // run webpack
+  webpack(webpackConfiguration, function(err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err);
+    gutil.log('[webpack]', stats.toString({}));
+    callback();
+  });
 });
 
+gulp.task('client', ['webpack']);
+
+/*** Backend ***/
 gulp.task('babel', function() {
   return gulp.src(['src/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('lib'));
 });
 
-gulp.task('buildTest', function() {
-  return gulp.src(['test/**/*.js'])
-    .pipe(babel())
-    .pipe(gulp.dest('lib-test'));
+gulp.task('copy', function() {
+  return gulp.src(['src/**/*', '!src/**/*.js'])
+    .pipe(gulp.dest('lib'));
 });
 
-gulp.task('default', ['babel']);
+gulp.task('server', ['babel', 'copy']);
+
+/*** Wrap up ***/
+
+gulp.task('watch', function() {
+  gulp.watch(['src/**', 'test/**'], ['mochaMin']);
+});
+
+gulp.task('clean', function() {
+  return del([
+    'dist/**/*',
+    'lib/**/*'
+  ]);
+});
+
+gulp.task('default', ['test', 'client', 'server']);
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import MessageServer from './messageServer';
 import apiRouter from './api';
 import logging from './util/logging';
 import errorCode from './util/errorCode';
+import serveStatic from 'serve-static';
 
 import networkConfig from '../config/network.config';
 
@@ -17,8 +18,40 @@ const app = express();
 app.use(logging);
 app.use('/api', apiRouter);
 
-app.all('/', (req, res) => {
-  res.send('IoTLogic central messaging server');
+if (__DEVELOPMENT__) {
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('../webpack.config.js');
+  let compiler = webpack(webpackConfig);
+  app.use(webpackHotMiddleware(compiler));
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: '/assets'
+  }));
+} else {
+  app.use('/assets', serveStatic('./dist'));
+}
+
+app.get('/favicon.ico', (req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta charset="UTF-8">
+        <title>IoTLogic</title>
+      </head>
+      <body>
+        <script src="/assets/bundle.js"></script>
+      </body>
+    </html>
+  `);
 });
 
 app.use(function(err, req, res, next) { // eslint-disable-line
